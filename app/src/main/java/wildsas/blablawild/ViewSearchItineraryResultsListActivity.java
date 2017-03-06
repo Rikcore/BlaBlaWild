cd17b8b408fb24a069be8a0a165723b2fc8f1497
[Rikcore] <[cyprien.chamayou@gmail.com]>
2017-03-06 11:58:57 +0100

    ajout date toast

--- app/src/main/java/wildsas/blablawild/ViewSearchItineraryResultsListActivity.java
+++ app/src/main/java/wildsas/blablawild/ViewSearchItineraryResultsListActivity.java
@@ -3,6 +3,7 @@ package wildsas.blablawild;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ViewSearchItineraryResultsListActivity extends AppCompatActivity {
 
@@ -18,6 +19,9 @@ public class ViewSearchItineraryResultsListActivity extends AppCompatActivity {
         Intent intentGoResults = getIntent();
         informationsclasse trajet = getIntent().getExtras().getParcelable(SearchItineraryActivity.EXTRA_TRAJET);
         this.setTitle(trajet.getmDepart() + " " + trajet.getmDestination() + " " + trajet.getmDate());
+
+        Toast.makeText(ViewSearchItineraryResultsListActivity.this, getString(R.string.felicitations) + " " + trajet.getmDepart() + " " + getString(R.string.le) + " " + trajet.getmDate(),
+                Toast.LENGTH_LONG).show();
 
 
     }
